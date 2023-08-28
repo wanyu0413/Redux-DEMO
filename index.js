4fb4cfc15da67faa64f9fcd4a6891ad5a376cd04
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-28 12:38:21 +0900

    action, reducer

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("From index.js")
+const CAKE_ORDERED = "CAKE_ORDERED"
+
+function orderCake(){
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1,
+    }
+}
+
+// (previousState, action) => newState
+
